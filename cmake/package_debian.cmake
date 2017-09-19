a2398d587ea6bcd7f62ce223828a5e086ddd49c9
Raphael Schaller <raphael.schaller@roboception.de>
2017-09-19 15:18:13 +0200

    disable RPF

--- cmake/package_debian.cmake
+++ cmake/package_debian.cmake
@@ -92,45 +92,45 @@ if (NOT DEFINED CPACK_DEBIAN_PACKAGE_SHLIBDEPS)
     set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
 endif ()
 
-# ??? this defaults to PROJECT_LIBRARIES which should be defined in main CMakeLists.txt before including this file
-# list of shared library this package provides (; separated, comment or empty list if there are none)
-# IMPORTANT: also the lib needs to set SOVERSION via set_target_properties, e.g.:
-# set_target_properties(rcimage PROPERTIES SOVERSION ${abiversion})
-if (PROJECT_LIBRARIES)
-    set(sharedlibs ${PROJECT_LIBRARIES})
-endif ()
+## ??? this defaults to PROJECT_LIBRARIES which should be defined in main CMakeLists.txt before including this file
+## list of shared library this package provides (; separated, comment or empty list if there are none)
+## IMPORTANT: also the lib needs to set SOVERSION via set_target_properties, e.g.:
+## set_target_properties(rcimage PROPERTIES SOVERSION ${abiversion})
+#if (PROJECT_LIBRARIES)
+#    set(sharedlibs ${PROJECT_LIBRARIES})
+#endif ()
 
-# if there are shared libs exported by this package:
-# generate debian shlibs file and call ldconf in postinst and postrm scripts
-if (sharedlibs)
-    set(SHLIBS_FILE "${CMAKE_CURRENT_BINARY_DIR}/shlibs")
-    set(POSTINST_SCRIPT "${CMAKE_CURRENT_BINARY_DIR}/postinst")
-    set(POSTRM_SCRIPT "${CMAKE_CURRENT_BINARY_DIR}/postrm")
+## if there are shared libs exported by this package:
+## generate debian shlibs file and call ldconf in postinst and postrm scripts
+#if (sharedlibs)
+#    set(SHLIBS_FILE "${CMAKE_CURRENT_BINARY_DIR}/shlibs")
+#    set(POSTINST_SCRIPT "${CMAKE_CURRENT_BINARY_DIR}/postinst")
+#    set(POSTRM_SCRIPT "${CMAKE_CURRENT_BINARY_DIR}/postrm")
 
-    # Generate postinst, prerm and postrm hooks
-    file(WRITE "${POSTINST_SCRIPT}" "#!/bin/sh\n\nset -e\n")
-    file(WRITE "${POSTRM_SCRIPT}" "#!/bin/sh\n\nset -e\n")
-    file(APPEND "${POSTINST_SCRIPT}" "if [ \"$1\" = \"configure\" ]; then\n        ldconfig\nfi\n")
-    file(APPEND "${POSTRM_SCRIPT}" "if [ \"$1\" = \"remove\" ]; then\n        ldconfig\nfi\n")
+#    # Generate postinst, prerm and postrm hooks
+#    file(WRITE "${POSTINST_SCRIPT}" "#!/bin/sh\n\nset -e\n")
+#    file(WRITE "${POSTRM_SCRIPT}" "#!/bin/sh\n\nset -e\n")
+#    file(APPEND "${POSTINST_SCRIPT}" "if [ \"$1\" = \"configure\" ]; then\n        ldconfig\nfi\n")
+#    file(APPEND "${POSTRM_SCRIPT}" "if [ \"$1\" = \"remove\" ]; then\n        ldconfig\nfi\n")
 
-    # Generate shlibs file
-    # also the lib needs to set SOVERSION via set_target_properties:
-    # set_target_properties(rcimage PROPERTIES SOVERSION ${abiversion})
-    file(WRITE "${SHLIBS_FILE}" "")
-    foreach (libname ${sharedlibs})
-        get_target_property(so_abiversion ${libname} SOVERSION)
-        if(NOT ${so_abiversion})
-          set(so_abiversion ${abiversion})
-          message(STATUS "SOVERSION of shared lib \"${libname}\" not set explicitly. Using <Major.Minor> of latest tag: ${so_abiversion}")
-          set_target_properties(${libname} PROPERTIES SOVERSION ${so_abiversion})
-        endif()
-        file(APPEND "${SHLIBS_FILE}" "lib${libname} ${so_abiversion} ${CPACK_PACKAGE_NAME}\n")
-    endforeach (libname)
+#    # Generate shlibs file
+#    # also the lib needs to set SOVERSION via set_target_properties:
+#    # set_target_properties(rcimage PROPERTIES SOVERSION ${abiversion})
+#    file(WRITE "${SHLIBS_FILE}" "")
+#    foreach (libname ${sharedlibs})
+#        get_target_property(so_abiversion ${libname} SOVERSION)
+#        if(NOT ${so_abiversion})
+#          set(so_abiversion ${abiversion})
+#          message(STATUS "SOVERSION of shared lib \"${libname}\" not set explicitly. Using <Major.Minor> of latest tag: ${so_abiversion}")
+#          set_target_properties(${libname} PROPERTIES SOVERSION ${so_abiversion})
+#        endif()
+#        file(APPEND "${SHLIBS_FILE}" "lib${libname} ${so_abiversion} ${CPACK_PACKAGE_NAME}\n")
+#    endforeach (libname)
 
-    execute_process(COMMAND chmod 644 "${SHLIBS_FILE}")
-    execute_process(COMMAND chmod 755 "${POSTINST_SCRIPT}" "${POSTRM_SCRIPT}")
-    set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${SHLIBS_FILE};${POSTINST_SCRIPT};${POSTRM_SCRIPT}")
-endif ()
+#    execute_process(COMMAND chmod 644 "${SHLIBS_FILE}")
+#    execute_process(COMMAND chmod 755 "${POSTINST_SCRIPT}" "${POSTRM_SCRIPT}")
+#    set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${SHLIBS_FILE};${POSTINST_SCRIPT};${POSTRM_SCRIPT}")
+#endif ()
 
 if (conffiles)
   set(CONFFILES_FILE "${CMAKE_CURRENT_BINARY_DIR}/conffiles")
