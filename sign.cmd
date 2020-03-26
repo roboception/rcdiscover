913ed83b485fdd755a816b6699a1ccf07bbeb402
Raphael Schaller <raphael.schaller@roboception.de>
2020-03-26 14:39:16 +0100

    switch to different time stamping service for code signing

--- sign.cmd
+++ sign.cmd
@@ -5,6 +5,6 @@ set pwd=%cd%
 
 pushd \\lina\codelicense
 
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x64\signtool.exe" sign /fd SHA512 /f Roboception_GmbH.p12 /p %1 /tr http://tsa.swisssign.net "%pwd%\rcdiscover.exe"" %pwd%\rcdiscover-gui.exe"
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x64\signtool.exe" sign /fd SHA512 /f Roboception_GmbH.p12 /p %1 /tr http://zeitstempel.dfn.de "%pwd%\rcdiscover.exe"" %pwd%\rcdiscover-gui.exe"
 
 popd
